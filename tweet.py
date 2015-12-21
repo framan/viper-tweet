3f5ab3a098252b0189ed935daac0c7c005376b22
Francesco Manetti <info@framan.io>
2015-12-22 00:06:42 +0100

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,30 +1,19 @@
 ################################################################################
 # Tweet with Viper
 #
-# Created by 123go.it
+# Created by library.123go.it
 # Authors: FraMan - SPira
 ################################################################################
 
-def tweet(twitter_id,public_token,auth_token,message):
+def tweet(public_token,secure_code,message):
         try:
-                if(twitter_id)!="":
-                        print("TWEET: Connecting to library.123go.it ...")    
-                        replaced_twitterid=twitter_id.replace("@","")
-                        nospace_message=message.replace(" ","%20")
-                        def_message=nospace_message+" @"+replaced_twitterid.strip()
-                        print("Using Token: "+public_token)
-                        print("Sending Tweet: "+def_message)
-                        url="http://library.123go.it/post.php"
-                        parameters={"tk":public_token, "msg":def_message, "auth_token":auth_token}
-                        response = requests.get(url,params=parameters)
-                else:
-                        print("TWEET: Connecting to library.123go.it ...")
-                        def_message=message.replace(" ","%20")
-                        print("Using Token: "+public_token)
-                        print("Sending Tweet: "+def_message)
-                        url="http://library.123go.it/post.php"
-                        parameters={"tk":public_token, "msg":def_message, "auth_token":auth_token}
-                        response = requests.get(url,params=parameters)
+                print("TWEET: Connecting to library.123go.it ...")
+                def_message=message.replace(" ","%20")
+                print("Using Token: "+public_token)
+                print("Sending Tweet: "+def_message)
+                url="http://library.123go.it/post.php"
+                parameters={"tk":public_token, "msg":def_message, "auth_token":secure_code}
+                response = requests.get(url,params=parameters)
         except Exception as e:
                 print(e)
 
